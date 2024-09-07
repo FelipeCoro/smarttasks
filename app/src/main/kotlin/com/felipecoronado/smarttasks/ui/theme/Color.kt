cd81992b97d1735968c82d344236fc1102505eeb
felipecoro <f.coronado838@gmail.com>
2024-09-07 08:39:41 -0500

    Folder structure, colors, fonts and resources setup.

---
+++ app/src/main/kotlin/com/felipecoronado/smarttasks/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.felipecoronado.smarttasks.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val YellowMain = Color(0xFFFFDE61)
+val RedMain = Color(0xFFEF4B5E)
+val GreenMain = Color(0xFF0E6878)
+val BeigeMain = Color(0xFFF6EFDE)
--- app/src/main/kotlin/com/felipecoronado/smarttasks/ui/theme/Type.kt
+++ app/src/main/kotlin/com/felipecoronado/smarttasks/ui/theme/Type.kt
@@ -2,33 +2,34 @@ package com.felipecoronado.smarttasks.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.felipecoronado.smarttasks.R
+
+
+val amsiProFamily = FontFamily(
+    Font(R.font.amsipro_regular, FontWeight.Normal),
+    Font(R.font.amsipro_bold, FontWeight.Bold),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = amsiProFamily,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 10.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
+
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
+        fontFamily = amsiProFamily,
+        fontWeight = FontWeight.Bold,
+        fontSize = 15.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+
 )
